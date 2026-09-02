5a8e5a984619ec171d0f6d5bbb776faae5bd1460
CODWARRIORHQ <ivanvisa8@gmail.com>
2026-09-02 18:06:03 +0200

    Add files via upload

--- index/supabase-config.js
+++ index/supabase-config.js
@@ -1,4 +1,4 @@
 window.CAMO_SUPABASE_CONFIG = {
     url: 'https://mpvahadjrrxrihzjzdpf.supabase.co',
-    anonKey: 'sb_publishable_ThwjYNlSLkW29xbu75l4Sg_IzMcGuNM'
+    anonKey: 'sb_publishable_t1hBo3rw4n9TScqY-1E8Yg_FK6PPMcv'
 };
